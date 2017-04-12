d1601b7b4712fbfaecb1ef5f48e27b4c5b738dfa
James Feore <jjfeore@gmail.com>
2017-04-12 00:54:12 -0700

    Added a couple portfolio items and prepping carousel

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ function PortfolioItem(name, description, url, pic) {
   this.url = url;
   this.pic = pic;
 }
+
+new PortfolioItem('Four in a Row', 'My final project for Code Fellows 201 was just a simple and clean replica of the Four in a Row game we\'re all familiar with.', 'https://jjfeore.github.io/fourinarow/', 'images/fourinarow.jpg');
+new PortfolioItem('Seattle Online Broadcasters Assc.', 'I\'ve helped build one of the largest and most active online broadcasting non-profits in the nation. SOBA dedicates itself to assisting all broadcasters in the Puget Sound area.', 'https://www.meetup.com/SOBA-Meetup/', 'images/soba.jpeg');
+new PortfolioItem('BanzaiBaby', 'As BanzaiBaby\'s channel and community manager, I\'ve helped build her channel to being among the most well-recognized on all of Twitch Creative.', 'https://www.twitch.tv/banzaibaby', 'images/banzaibaby.png');
